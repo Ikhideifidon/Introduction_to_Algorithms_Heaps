4c83068052388484e657fe6e422b08f5c2e27754
ifidonikhide <ikhideifidon@yahoo.com>
2023-01-18 12:56:47 -0500

    Heaps

--- src/main/java/com/github/Ikhideifidon/Main.java
+++ src/main/java/com/github/Ikhideifidon/Main.java
@@ -1,5 +1,8 @@
-package com.github.Ikhideifidon;public class Main {
+package com.github.Ikhideifidon;
+
+public class Main {
     public static void main(String[] args) {
+        System.out.println(1 + (5 << 1));
         System.out.println("Hello world!");
     }
 }
